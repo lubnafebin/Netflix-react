096decf8f3363288ef1a0e63ed856b826580cde1
Lubna Febin <lubnafebin13@gmail.com>
2025-10-23 20:10:28 +0530

    Enhance Navbar functionality with scroll detection; update styles for improved layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${show ? "navbar--solid" : ""}`}>
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         alt="Netflix logo"
