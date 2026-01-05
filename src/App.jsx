a7d245cae09a4ec843532670357f860e3c97ce42
Lubna Febin <lubnafebin13@gmail.com>
2026-01-05 21:02:58 +0530

    Refactor App component to use Routes for improved routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,32 @@ import {
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 import RowPost from "./components/RowPost/RowPost";
+import { Route, Routes } from "react-router-dom";
+import { MovieDetails } from "./MovieDetails";
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Banner />
-      <RowPost url={originals} title="Netflix Original" />
-      <RowPost url={action} title="Action" isSmall />
-      <RowPost url={ComedyMovies} title="ComedyMovies" isSmall />
-      <RowPost url={HorrorMovies} title="HorrorMovies" isSmall />
-      <RowPost url={RomanceMovies} title="RomanceMovies" isSmall />
-      <RowPost url={Documentaries} title="Documentaries" isSmall />
-    </div>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <RowPost url={originals} title="Netflix Original" />
+              <RowPost url={action} title="Action" isSmall />
+              <RowPost url={ComedyMovies} title="ComedyMovies" isSmall />
+              <RowPost url={HorrorMovies} title="HorrorMovies" isSmall />
+              <RowPost url={RomanceMovies} title="RomanceMovies" isSmall />
+              <RowPost url={Documentaries} title="Documentaries" isSmall />
+            </>
+          }
+        />
+
+        <Route path="/details/:id" element={<MovieDetails />} />
+      </Routes>
+    </>
   );
 }
 export default App;
