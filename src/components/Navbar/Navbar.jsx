f8d94c1e8d8e43656be0ef40a72b7de3029dcd97
Lubna Febin <lubnafebin13@gmail.com>
2026-01-06 21:05:49 +0530

    Add navigation functionality to Navbar logo for homepage redirection

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const [show, setShow] = useState(false);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 100) {
@@ -22,6 +23,7 @@ function Navbar() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         alt="Netflix logo"
         className="logo"
+        onClick={() => navigate("/")}
       />
       <img
         src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
